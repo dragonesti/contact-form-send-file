12b73fdb79f8e907a8e20c0735dcb28e4e1a10b0
Marcin Jóźwiak <dragonesti@poczta.onet.pl>
2022-03-24 13:56:07 +0100

    In class FormValidator add method to validate username and input file.

--- inc/FormValidator.php
+++ inc/FormValidator.php
@@ -6,5 +6,69 @@
 
 class FormValidator
 {
+    private $data;
+    private $file;
+    private $errors = [];
+    private static $data_fields = ['username'];
+    private static $file_fields = ['file'];
 
+    public function __construct($post_data, $file_data)
+    {
+        $this->data = $post_data;
+        $this->file = $file_data;
+    }
+
+    public function validateForm()
+    {
+
+//    echo '<pre>';
+//    var_dump($this->file['file']);
+//    echo '</pre>';
+//    echo '<pre>';
+//    var_dump($this->data['username']);
+//    echo '</pre>';
+
+        $this->validateUser();
+        $this->validateFile();
+        return $this->errors;
+    }
+
+    private function validateUser()
+    {
+
+        $val = trim($this->data['username']);
+        if(empty($val)){
+            $this->addErrors('username','username cannot be empty');
+        } else {
+            if (!preg_match('/^[a-zA-Z0-9]{6,12}$/',$val)){
+                $this->addErrors('username','username must be 6-12 char & alphanumeric ');
+            }
+        }
+    }
+
+    private function validateFile()
+    {
+        $file_new = $this->file['file'];
+        if($file_new['error']!==0){
+            $this->addErrors('file','Please add file to send');
+        } else {
+            $ext = pathinfo($file_new['name'], PATHINFO_EXTENSION);
+            $ext = strtolower($ext);
+
+            /* The size, in bytes, of the uploaded file. Below is set 3MB*/
+            if (($file_new['size'] > 3 * 1024 * 1024)) {
+                $this->addErrors('file','File is too large');
+            }
+            /* Check the upload file extension */
+            elseif (!in_array($ext, [ 'png','jpg', 'svg', 'gif', 'jpeg'])) {
+                $this->addErrors('file','Only images are allowed to upload');
+            }
+
+        }
+    }
+
+    private function addErrors($key, $val)
+    {
+        $this->errors[$key] = $val;
+    }
 }
--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
 require ('inc/FormValidator.php');
 if (isset($_POST['submit'])) {
-//    $validation = new UserValidator($_POST);
-//    $errors = $validation->validateForm();
+    $validation = new FormValidator($_POST, $_FILES);
+    $errors = $validation->validateForm();
 
-//    if (empty($errors)) {
-//        echo 'Form validation is ok, Next step is doing something with form data';
-//    }
+    if (empty($errors)) {
+        echo 'Form validation is ok, Next step is doing something with form data';
+    }
+
+//    echo '<pre>';
+//    var_dump($_FILES['file']);
+//    echo '</pre>';
 }
 
 ?>
@@ -21,20 +25,20 @@ if (isset($_POST['submit'])) {
 </head>
 <body>
     <section id="contact__section">
-        <h1 class="contact__form--header">Send file form</h1>
+        <h1 class="contact__form--header">Send file</h1>
         <hr class="dec">
         <span class="msh">
-<!--            --><?php
-//                if (isset($message)) {
-//                    echo $message;
-//                }
-//            ?>
+            <?php
+                if (isset($errors)) {
+                    print_r ($errors);
+                }
+            ?>
         </span>
         <hr class="dec">
-        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data" action="">
-            <input name="text" type="name" id="name" placeholder="Write your name"/>
-            <br><input name="file" type="file" id="file"/>
-            <br><button name="send">Send </button>
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data" >
+            <input name="username" type="text" placeholder="Write your name" value="<?php echo htmlspecialchars($_POST['username'] ?? ''); ?>"/>
+            <br><input name="file" type="file"  />
+            <br><button type="submit" name="submit"> Send </button>
         </form>
     </section>
 </body>
